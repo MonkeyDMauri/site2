0c8a1414314fe55a1decd7ba99603e06b21257b5
mauricio.rodriguez <maurirodriguezm48@gmail.com>
2025-01-11 23:21:25 -0600

    initial commit

---
+++ signin_f/signin_page.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signin Page</title>
+    <link rel="stylesheet" href="./signin_style.css">
+</head>
+<body>
+
+    <div class="main-wrapper">
+        <div class="main-wrap">
+            <h1>Maubook</h1>
+        </div>
+    </div>
+    
+</body>
+</html>
